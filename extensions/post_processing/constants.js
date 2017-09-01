9bde457df2d599b777dee3601e4947ebe223655a
kydavis <kdavis@e1z3r3p2.42.us.org>
2017-09-01 11:49:38 -0700

    ft: Clone replication directory to post_processing
    Create a workspace for post_processing. Uses
    replication as a template for queue populating and
    processing.

---
+++ extensions/post_processing/constants.js
@@ -0,0 +1,9 @@
+'use strict'; // eslint-disable-line
+
+const constants = {
+    zookeeperReplicationNamespace: '/backbeat/replication',
+    proxyPath: '/_/backbeat/vault',
+    proxyIAMPath: '/_/backbeat/iam',
+};
+
+module.exports = constants;
